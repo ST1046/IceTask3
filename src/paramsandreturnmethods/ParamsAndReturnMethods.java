1f78b2b0750d74bc4bb318dc2071fb53b911644e
Debbie <164020351+ST1046@users.noreply.github.com>
2025-03-18 08:54:28 +0200

    Initial commit

---
+++ src/paramsandreturnmethods/ParamsAndReturnMethods.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paramsandreturnmethods;
+
+/**
+ *
+ * @author lab_services_student
+ */
+import javax.swing.JOptionPane;
+public class ParamsAndReturnMethods {
+
+   
+    public static double calculateTotal(double price1, double price2, double price3){
+        return price1 + price2 + price3;
+    }
+    
+            
+    public static void main(String[] args) {
+        String input1 = JOptionPane.showInputDialog("Price of Product 1: ");
+        String input2 = JOptionPane.showInputDialog("Price of Product 2: ");
+        String input3 = JOptionPane.showInputDialog("Price of Product 3: ");
+        
+        double price1 = Double.parseDouble(input1);
+        double price2 = Double.parseDouble(input2);
+        double price3 = Double.parseDouble(input3);
+        
+        double totalCost = calculateTotal(price1, price2, price3);
+        
+        JOptionPane.showMessageDialog(null, "Total cost is: R" + totalCost);
+    }
+    
+}
